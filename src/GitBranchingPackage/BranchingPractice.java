725012477834bcd60966ee6d655edc686ca0d87c
unknown <faisal.amanzai@tekschool.us>
2022-11-08 18:29:01 -0800

    git branching commit

---
+++ src/GitBranchingPackage/BranchingPractice.java
@@ -0,0 +1,12 @@
+package GitBranchingPackage;
+
+public class BranchingPractice {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is Branching practice");
+		
+
+	}
+
+}
